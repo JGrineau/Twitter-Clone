a1ed0dd0e8b0d0fe1397042895a59fcfc19acbac
JGrineau <jgrineau95@gmail.com>
2023-11-08 00:36:11 -0500

    Left side bar 99% done

--- twitter-clone/src/app/page.tsx
+++ twitter-clone/src/app/page.tsx
@@ -1,6 +1,6 @@
 // import React from "react";
 
-import { BsFillHouseDoorFill } from "react-icons/bs";
+import { AiOutlineHome } from "react-icons/ai";
 import { HiOutlineHashtag } from "react-icons/hi";
 import { HiOutlineBell } from "react-icons/hi";
 import { HiOutlineEnvelope } from "react-icons/hi2";
@@ -8,10 +8,12 @@ import { RiFileListLine } from "react-icons/ri";
 import { BsBookmark } from "react-icons/bs";
 import { CiUser } from "react-icons/ci";
 import { FaSquareXTwitter } from "react-icons/fa6";
+import { BsThreeDots } from "react-icons/bs";
 import Link from "next/link";
 
 const NAVIGATION_ITEMS = [
-  { title: "Home", icon: BsFillHouseDoorFill },
+  { title: "X", icon: FaSquareXTwitter },
+  { title: "Home", icon: AiOutlineHome },
   { title: "Explore", icon: HiOutlineHashtag },
   { title: "Notifications", icon: HiOutlineBell },
   { title: "Messages", icon: HiOutlineEnvelope },
@@ -25,22 +27,34 @@ const home = () => {
     <div className="w-full h-full flex justify-center items-center relative bg-black">
       <div className="max-w-screen-lg w-full h-full flex relative">
         {/* Left Side bar  */}
-        <section className="fixed w-72 flex flex-col">
-          <Link href={"/"}>
-            <FaSquareXTwitter />
-          </Link>
-          {NAVIGATION_ITEMS.map((item) => (
-            <Link
-              className="hover:bg-white/20 transition duration-200 flex items-center justify-start w-fit justify-center space-x-2 rounded-3xl p-4"
-              href={`/${item.title.toLowerCase()}`}
-              key={item.title}
-            >
-              <div>
-                <item.icon />
-              </div>
-              <div>{item.title}</div>
-            </Link>
-          ))}
+        <section className="fixed w-72 flex flex-col h-screen space-y-4 items-stretch">
+          <div className="flex flex-col h-screen space-y-4 items-stretch">
+            {NAVIGATION_ITEMS.map((item) => (
+              <Link
+                className="hover:bg-white/10 transition duration-200 flex items-center text-2xl justify-start w-fit space-x-2 rounded-3xl py-2 px-6"
+                href={`/${item.title.toLowerCase()}`}
+                key={item.title}
+              >
+                <div>
+                  <item.icon />
+                </div>
+                {item.title !== "X" && <div>{item.title}</div>}
+              </Link>
+            ))}
+            <button className="w-full rounded-full bg-primary p-4 m-4 text-center hover:bg-opacity-70 transition duration-200">
+              Post
+            </button>
+          </div>
+          <button className="w-full rounded-full flex items-center space-x-2 bg-transparent p-4 m-4 text-center hover:bg-white/10 transition duration-200">
+            <div className="rounded-full bg-slate-400 w-8 h-8 "></div>
+            <div className="text-left text-xs">
+              <div className="font-semibold">Club of JP Grineau</div>
+              <div className="">@JP_Grineau</div>
+            </div>
+            <div>
+              <BsThreeDots />
+            </div>
+          </button>
         </section>
         {/* <main>Home Timeline</main>
         <section>right sidebar</section> */}
