7e080f9d919f18e928b4206695fce7e415bfd8bb
JGrineau <jgrineau95@gmail.com>
2023-11-13 23:43:30 -0500

    updated the main component

--- twitter-clone/src/app/page.tsx
+++ twitter-clone/src/app/page.tsx
@@ -1,5 +1,6 @@
 // import React from "react";
 import LeftSidebar from "./components/leftSideBar";
+import MainComponent from "./components/main-component";
 
 const home = () => {
   return (
@@ -7,10 +8,8 @@ const home = () => {
       <div className="max-w-screen-lg w-full h-full flex relative">
         {/* Left Side bar  */}
         <LeftSidebar />
-
-        <main className="ml-[275px] p-5 mx-6 flex w-[600px] min-h-screen g-full flex-col border-l-[0.5px] border-r-[0.5px] border-gray-500">
-          <h1 className="text-2xl font-bold">Home</h1>
-        </main>
+        <MainComponent />
+        {/* <main className="ml-[275px] p-5 mx-6 flex w-[600px] min-h-screen g-full flex-col border-l-[0.5px] border-r-[0.5px] border-gray-500"></main> */}
         {/*<section>right sidebar</section> */}
       </div>
     </div>
